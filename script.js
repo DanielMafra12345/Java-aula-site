6b621982660dfeeeef99869f4b2d4f188e57a791
DanielMafra12345 <daniel.mafra.pinto@escola.pr.gov.br>
2026-06-08 11:45:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function profissao(){
+    document.getElementById("info").innerHTML = "<p>Sou engenheiro robótico e desenvolvedor, Sou dono de uma empresa de tecnologia</p>"
+}
 
+function infobasica(){
+    document.getElementById("info").innerHTML = " <p>Tenho 12 anos, Quando estava no quinto ano eu era o mais inteligente da escola</p>"
+}
+
+function hobbies(){
+    document.getElementById("info").innerHTML = "  <p>Eu gosto de progamar e desenvolver códigos, gosto de jogar Minecraft e assistir TV</p>"
+}
